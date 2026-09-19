7bcd659376edb22af5e648094c96f9e6d56c391c
Tabitha2005 <179332705+Tabitha2005@users.noreply.github.com>
2026-09-19 23:14:23 +0100

    Add Header component with logo and New Post link

---
+++ mini-blog/src/components/Header.tsx
@@ -0,0 +1,14 @@
+function Header() {
+  return (
+    <header className="header">
+      <div className="header-logo">Dev Insights</div>
+      <nav className="header-nav">
+        <a href="#new-post" className="header-link">
+          New Post
+        </a>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
