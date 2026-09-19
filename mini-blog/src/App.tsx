9bd4ecbe00438976587d7615db4c37451a15eff4
Tabitha2005 <179332705+Tabitha2005@users.noreply.github.com>
2026-09-19 23:25:41 +0100

    Render Header above PostList in App

--- mini-blog/src/App.tsx
+++ mini-blog/src/App.tsx
@@ -1,11 +1,16 @@
+import Header from './components/Header'
 import PostList from './components/PostList'
 import { posts } from './data/posts'
 
+
 function App() {
   return (
-    <main>
-      <PostList posts={posts} />
-    </main>
+    <>
+      <Header />
+      <main>
+        <PostList posts={posts} />
+      </main>
+    </>
   )
 }
 
