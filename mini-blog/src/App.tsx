ded81736343154142b30b2b2bc206aafe3c8aa39
Tabitha2005 <179332705+Tabitha2005@users.noreply.github.com>
2026-09-19 23:31:08 +0100

    Add layout wrapper and page heading in App

--- mini-blog/src/App.tsx
+++ mini-blog/src/App.tsx
@@ -2,15 +2,15 @@ import Header from './components/Header'
 import PostList from './components/PostList'
 import { posts } from './data/posts'
 
-
 function App() {
   return (
-    <>
+    <div className="app">
       <Header />
-      <main>
+      <main className="app-main">
+        <h1 className="app-heading">Latest posts</h1>
         <PostList posts={posts} />
       </main>
-    </>
+    </div>
   )
 }
 
